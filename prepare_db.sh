f81ea168f1d3bb9b56100bf8105451f58aef83d8
Viraat Chandra <viraat.chandra@hotmail.com>
2018-12-07 17:13:53 +0530

    updated start.sh and added prepare_db.sh scripts

---
+++ prepare_db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+_red_=`tput setaf 1`
+_green_=`tput setaf 2`
+_reset_=`tput sgr0`
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+echo "${_green_}running database migrations${_reset_}"
+python "$DIR/Smarty/manage.py" make_migrations
+python "$DIR/Smarty/manage.py" migrate
--- start.sh
+++ start.sh
@@ -7,5 +7,8 @@ _reset_=`tput sgr0`
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
+echo "${_green_}setting up database${_reset_}"
+source "$DIR/prepare_db.sh"
+
 echo "${_green_}starting the server${_reset_}"
 python "$DIR/Smarty/manage.py" runserver
