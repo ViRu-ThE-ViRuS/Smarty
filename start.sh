0c10af7b76cbcfa8a177542567ebee7d349dcfe4
Viraat Chandra <viraat.chandra@hotmail.com>
2018-12-07 17:09:26 +0530

    updated start.sh script

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@ _red_=`tput setaf 1`
 _green_=`tput setaf 2`
 _reset_=`tput sgr0`
 
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
 echo "${_green_}starting the server${_reset_}"
-python Smarty/manage.py runserver
+python "$DIR/Smarty/manage.py" runserver
