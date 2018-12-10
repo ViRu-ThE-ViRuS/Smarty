649ea49f54681e2978b7024f09b8b511ff92e678
Viraat Chandra <viraat.chandra@hotmail.com>
2018-12-10 17:22:49 +0530

    added formatting and documentations

--- Smarty/main/apps.py
+++ Smarty/main/apps.py
@@ -1,5 +1,12 @@
+"""
+main.apps.py
+"""
+
 from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
+    """
+    Main App Configuration
+    """
     name = 'main'
--- Smarty/main/views.py
+++ Smarty/main/views.py
@@ -1,5 +1,15 @@
+"""
+main.views.py
+"""
+
 from django.shortcuts import render
 
 
 def index_view(request):
+    """
+    handle request on '/'
+
+    :param request: http request
+    :return: rendered view
+    """
     return render(request, 'index.html', {})
